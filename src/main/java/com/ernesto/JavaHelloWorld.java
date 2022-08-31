dc5aebd42f1289bf9029e6dc9da69729784511b5
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-08-31 13:19:48 -0500

    configure initial status of files

---
+++ src/main/java/com/ernesto/JavaHelloWorld.java
@@ -0,0 +1,7 @@
+package com.ernesto;
+
+public class JavaHelloWorld {
+    public String getHello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/ernesto/JavaHelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.ernesto;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        System.out.println(new JavaHelloWorld().getHello());
+    }
+}
